3cbe72104cd179f1ab76ecc07a0fb0a39c2f4306
Amid-1 <amid.1spbtj@gmail.com.com>
2025-04-06 12:44:40 +0300

    Initial commit

---
+++ src/main/java/ru/kata/spring/boot_security/demo/controller/UserRestController.java
@@ -0,0 +1,52 @@
+package ru.kata.spring.boot_security.demo.controller;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import ru.kata.spring.boot_security.demo.dto.UserDto;
+import ru.kata.spring.boot_security.demo.dto.UserFormCreateDto;
+import ru.kata.spring.boot_security.demo.dto.UserFormUpdateDto;
+import ru.kata.spring.boot_security.demo.service.UserService;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/users")
+public class UserRestController {
+
+    private final UserService userService;
+
+    public UserRestController(UserService userService) {
+        this.userService = userService;
+    }
+
+    @GetMapping
+    public ResponseEntity<List<UserDto>> getAllUsers() {
+        return ResponseEntity.ok(userService.getAllUsers());
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<UserDto> getUserById(@PathVariable Long id) {
+        return ResponseEntity.ok(userService.getUserById(id));
+    }
+
+    @PostMapping
+    public ResponseEntity<Void> createUser(@RequestBody UserFormCreateDto dto) {
+        userService.createUser(dto);
+        return ResponseEntity.ok().build();
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<Void> updateUser(@PathVariable Long id,
+                                           @RequestBody UserFormUpdateDto dto) {
+        dto.setId(id);
+        userService.updateUser(dto);
+        return ResponseEntity.ok().build();
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> deleteUser(@PathVariable Long id) {
+        userService.deleteUser(id);
+        return ResponseEntity.ok().build();
+    }
+}
+
